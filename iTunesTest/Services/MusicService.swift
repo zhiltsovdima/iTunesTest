84706148620f3993f216d573fe0cc3403d5e5efd
Dima Zhiltsov <zhiltsovdima@gmail.com>
2023-05-30 20:30:35 +0300

    MusicService a little refactoring

--- iTunesTest/Services/MusicService.swift
+++ iTunesTest/Services/MusicService.swift
@@ -14,19 +14,32 @@ protocol MusicServiceProtocol {
 final class MusicService: MusicServiceProtocol {
     
     private let networkManager: NetworkManagerProtocol
+    private var searchTask: DispatchWorkItem?
     
     init(_ networkManager: NetworkManagerProtocol) {
         self.networkManager = networkManager
     }
     
     func getMusic(by name: String, completion: @escaping (Result<[SongDataModel], NetworkError>) -> Void) {
+        searchTask?.cancel()
+        let newSearchTask = DispatchWorkItem { [weak self] in
+            guard let self else { return }
+            self.performSearch(by: name, completion: completion)
+        }
+        searchTask = newSearchTask
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: newSearchTask)
+    }
+
+    private func performSearch(
+        by name: String,
+        completion: @escaping (Result<[SongDataModel], NetworkError>) -> Void
+    ) {
         networkManager.fetchData(requestType: .songs(name)) { (result: Result<ResultsDataModel, NetworkError>) in
             switch result {
             case .success(let resultsData):
                 let music = resultsData.results
                 completion(.success(music))
             case .failure(let netError):
-                print(netError.description)
                 completion(.failure(netError))
             }
         }
